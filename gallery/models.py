15cbb7eb7440a1746e828acc81858c10ed9c28ea
Alex Cruz <Alexcruz182@gmail.com>
2018-06-07 18:50:44 -0400

    Changed DB and add home page

---
+++ gallery/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+# Creating objects for the database.
+class ArtWork(models.Model):
+    image = models.ImageField(upload_to='images/') #image for the picture
+    desc = models.CharField(max_length=200) #this is a descripton of the image
+
+
+                          
+                          
